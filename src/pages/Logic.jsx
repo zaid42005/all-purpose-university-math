cde52f854ff2c463444bbc998f79a462393045cd
zaid42005 <zaid.42005@gmail.com>
2024-03-02 23:12:46 -0500

    added basic template to Logic

--- src/pages/Logic.jsx
+++ src/pages/Logic.jsx
@@ -1,12 +1,32 @@
-//import { useState } from "react"
+import { useState } from "react"
 
 const Logic = () => {
-    //const [input,setInput] = useState("")
+    const [input, setInput] = useState("");
+  
+    const handleInputChange = (event) => {
+      const { value } = event.target;
+      setInput(value)
+    };
 
     return(
     <div className="container">
         <h2> Squished Logic says this is coming soon, call 1-800-273-8255 if ur really struggling without this</h2>
         <img src = "https://www.usmagazine.com/wp-content/uploads/2019/10/Logic-Welcomes-Baby-Boy-With-Brittany-Noell-After-Rapping-Announcement.jpg?w=1200&quality=40&strip=all" style={{width: "30vw", height: "30vh"}}></img>
+        <form>
+        <label>
+          y =  
+          <input
+            type="text"
+            name="a"
+            onChange={handleInputChange}
+            value={input}
+          />
+        </label>
+      </form>
+      <button onClick={ input }>
+        Evaluate
+      </button>
+
     </div>
     )
 }
