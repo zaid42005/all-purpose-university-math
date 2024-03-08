2d143e300f395c2e7b7fe1a035d3369d43492464
zaid42005 <zaid.42005@gmail.com>
2024-03-08 00:17:02 -0500

    no clue whats going on here with this vue stuff gonna learn it and be back

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+// frontend/src/App.vue
+
+<script>
+export default {
+  data() {
+    return {
+      query: 'x',
+      result: ''
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await fetch(`http://localhost:3000/wolfram?query=${this.query}`);
+        const data = await response.text();
+        this.result = data;
+      } catch (error) {
+        console.error('Error fetching data from backend:', error);
+      }
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
+};
+</script>
+
+<template>
+  <div>
+    <pre>{{ result }}</pre>
+  </div>
+</template>
